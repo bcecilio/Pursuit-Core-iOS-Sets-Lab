993fa97ccdab61805eac171728cb3e4d88878404
Brendon Cecilio <brendoncecilio@Brendons-MacBook-Pro.local>
2019-10-08 17:42:15 -0400

    initial answers

--- Pursuit-Core-iOS-Sets-Lab.playground/Contents.swift
+++ Pursuit-Core-iOS-Sets-Lab.playground/Contents.swift
@@ -8,23 +8,30 @@ import UIKit
 
 let numbers = [1,1,2,4,4,4,6,6,7,8]
 
-var numbersWithNoDuplicates = [Int]()
+var numbersWithNoDuplicates = Array(Set(numbers)).sorted()
 
-// Your code here
-
-//assert(numbersWithNoDuplicates == [1,2,4,6,7,8], "Was expecting [1,2,4,6,7,8], but got \(numbersWithNoDuplicates)")
+assert(numbersWithNoDuplicates == [1,2,4,6,7,8], "Was expecting [1,2,4,6,7,8], but got \(numbersWithNoDuplicates)")
 
 // Questions Two
 
 // Create a new array scoresThatAppearOnce that has all the elements from scores that appear exactly once.  It should be in the same order as the original.
 
 let scores = [1, 77, 83, 32, 77, 77, 83, 32, 99]
-
 var scoresThatAppearOnce = [Int]()
+var visitedNum: [Int] = []
 
-// Your code here
+for num in scores {
+  if !visitedNum.contains(num) {
+      scoresThatAppearOnce.append(num)
+      visitedNum.append(num)
+  } else {
+      if let index = scoresThatAppearOnce.firstIndex(of: num) {
+          scoresThatAppearOnce.remove(at: index)
+      }
+  }
+}
 
-//assert(scoresThatAppearOnce == [1, 99], "Was expecting [1, 99], but got \(scoresThatAppearOnce)")
+assert(scoresThatAppearOnce == [1, 99], "Was expecting [1, 99], but got \(scoresThatAppearOnce)")
 
 // Question Three
 
@@ -35,24 +42,20 @@ var scoresThatAppearOnce = [Int]()
 let arrOne = [1,2,3,4,5]
 let arrTwo = [3,4,5,6,7]
 
-var arrThree: [Int] = []
+var arrThree = Array(Set(arrOne + arrTwo)).sorted()
 
-// Your code here
-
-//assert(arrThree == [1,2,3,4,5,6,7], "Was expecting [1,2,3,4,5,6,7], but got \(arrThree)")
+assert(arrThree == [1,2,3,4,5,6,7], "Was expecting [1,2,3,4,5,6,7], but got \(arrThree)")
 
 // b.
 
 // Given arrFour and arrFive, create a variable arrSix with is equal to the INTERSECTION of arrFour and arrFive.  If should not contain any duplicate elements.  Sort arrSix from smallest to greatest.
 
-let arrFour = [1,2,3,4,5]
-let arrFive = [3,4,5,6,7]
+let arrFour = Set([1,2,3,4,5])
+let arrFive = Set([3,4,5,6,7])
 
-var arrSix: [Int] = []
+var arrSix = arrFour.intersection(arrFive)
 
-// Your code here
-
-//assert(arrSix == [3,4,5], "Was expecting [3,4,5], but got \(arrSix)")
+assert(arrSix == [3,4,5], "Was expecting [3,4,5], but got \(arrSix)")
 
 // Question Four
 
